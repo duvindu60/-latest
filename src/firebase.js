fabfd00cd2d15edfb2c0ee3c53a3192db7641df8
Duvindu Jayasinghe <duvindu60152370@gmail.com>
2025-01-16 17:43:00 +0530

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+   apiKey: "AIzaSyBodtW5B47yCKQzIsmYPrKQjqW6U4-Lrbs",
+   authDomain: "rfid-login-8425c.firebaseapp.com",
+   projectId: "rfid-login-8425c",
+   storageBucket: "rfid-login-8425c.firebasestorage.app",
+   messagingSenderId: "769875979265",
+   appId: "1:769875979265:web:f0bdffa7e13abcb7ada400",
+   measurementId: "G-D0369WEQ9F"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+
+
+
